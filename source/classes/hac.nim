43f42e3a7f3e3782af49a23b680bd237e028094c
Arham Jain <arhamjain@gmail.com>
2021-05-22 19:49:03 -0400

    finished converting over switch 3ds is next

--- source/classes/hac.nim
+++ source/classes/hac.nim
@@ -9,11 +9,16 @@ import strutils
 let meta_cmd = "nacptool --create '$1' '$2' $3 $4.nacp"
 let bin_cmd  = "elf2nro $1 $2.nro --icon=$3 --nacp=$2.nacp"
 
-type
-    HAC* = ref object of Console
+type HAC* = ref object of ConsoleBase
 
-method publish(self : HAC, source : string) : bool =
-    let binaryPath = fmt("{self.getBuildDirectory()}/{self.name}")
+proc getName*(self : HAC) : string = "Nintendo Switch"
+proc getProjectName*(self : HAC) : string = self.name
+proc getBinaryName*(self : HAC) : string = "Switch.elf"
+proc getIconExtension*(self: HAC) : string = "jpg"
+proc getExtension*(self : HAC) : string = "nro"
+
+proc publish(self : HAC, source : string) : bool =
+    let binaryPath = fmt("{getBuildDirectory()}/{self.name}")
 
     # Create metadata
     runCommand(meta_cmd.format(self.name, self.author, self.version, binaryPath))
@@ -23,5 +28,12 @@ method publish(self : HAC, source : string) : bool =
 
     return self.packGameDirectory(source)
 
-method getName(self : HAC) : string =
-    return "Nintendo Switch"
+when isMainModule:
+    import unittest
+    test "Name":
+        var switch = HAC(name: "my game", author: "me", description: "cool", version: "1.2")
+        check switch.getName() == "Nintendo Switch"
+        check switch.getProjectName() == "my game"
+        check switch.getBinaryName() == "Switch.elf"
+        check switch.getIconExtension() == "jpg"
+        check switch.getExtension() == "nro"
