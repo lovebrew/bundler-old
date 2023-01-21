3053b2baa2c1cb25e57fa88eb70a63247f12723b
Lexi <lexi@qixils.dev>
2023-01-21 13:27:19 -0500

    Wrap more command-line args in quotations (#24)
    * wrap more command-line args in quotations
    
    some filenames can be valid but require quotations to be properly handled
    this should, for example, fix apostrophes throwing "Syntax error: Unterminated quoted string"
    
    * use long-string literals

--- src/enums/command.nim
+++ src/enums/command.nim
@@ -6,14 +6,14 @@ import ../data/strings
 
 type
     Command* = enum
-        Tex3ds = "tex3ds $1 -o $2"
-        Mkbcfnt = "mkbcfnt $1 -o $2"
+        Tex3ds = """tex3ds "$1" -o "$2""""
+        Mkbcfnt = """mkbcfnt "$1" -o "$2""""
 
-        CtrUpdate = "hbupdater ctr --filepath $1 --title \"$2\" --author \"$3\" --description \"$4\" --iconPath $5 --output $6"
-        CtrUpdateNoIcon = "hbupdater ctr --filepath $1 --title \"$2\" --author \"$3\" --description \"$4\" --output $5"
+        CtrUpdate = """hbupdater ctr --filepath "$1" --title "$2" --author "$3" --description "$4" --iconPath "$5" --output "$6""""
+        CtrUpdateNoIcon = """hbupdater ctr --filepath "$1" --title "$2" --author "$3" --description "$4" --output "$5""""
 
-        HacUpdate = "hbupdater hac --filepath $1 --title \"$2\" --author \"$3\" --iconPath $4 --output $5"
-        HacUpdateNoIcon = "hbupdater hac --filepath $1 --title \"$2\" --author \"$3\" --output $4"
+        HacUpdate = """hbupdater hac --filepath "$1" --title "$2" --author "$3" --iconPath "$4" --output "$5""""
+        HacUpdateNoIcon = """hbupdater hac --filepath "$1" --title "$2" --author "$3" --output "$4""""
 
 proc run*(command: string, args: varargs[string, `$`]): bool =
     let commandString = command.format(args).replace("\\", "/")
